98a697ae2fe763b51dc4545ade01309249676c2b
Enzo Hamelin <enzo.hamelin@gmail.com>
2015-10-26 13:18:41 +0100

    responsive improvement

--- Gruntfile.js
+++ Gruntfile.js
@@ -344,6 +344,7 @@ module.exports = function (grunt) {
                     src: [
                         '*.{ico,png,txt}',
                         '.htaccess',
+		        'sitemap.xml',
                         '*.html',
                         'views/{,*/}*.html',
                         'images/{,*/}*.{webp}',

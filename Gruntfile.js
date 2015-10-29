4aa25a0a6a1045b0200963f5d8ba02ae1a0dc1a1
Enzo Hamelin <enzo.hamelin@gmail.com>
2015-10-29 23:33:00 +0100

    Correction base tag for html5mode

--- Gruntfile.js
+++ Gruntfile.js
@@ -78,7 +78,7 @@ module.exports = function (grunt) {
                     open: true,
                     middleware: function (connect) {
                     return [
-			    modRewrite(['!\\.html|\\.js|\\.svg|\\.css|\\.png|\\.jpg|\\.jpeg|\\.ttf*|\\.woff*$ /index.html [L]']),                     
+			    modRewrite(['^[^\\.]*$ /index.html [L]']),                     
                             connect.static('.tmp'),
                             connect().use(
                                 '/bower_components',
